2cb2c23ca72cb218970c1718998861f80c6ecac7
MeghaKhawadia <megha.mask1999@gmail.com>
2020-07-26 14:37:31 +0530

    third commit

--- sankalan/sankalan/urls.py
+++ sankalan/sankalan/urls.py
@@ -30,4 +30,12 @@ urlpatterns = [
     path('register/', views.register),
     path('check_user/', views.check_user, name="check_user"),
     path('user_logout/', views.user_logout, name="user_logout"),
+
+    # Hindi
+    path('hindi', views.index_hindi),
+    path('homepage_hindi', views.index_hindi),
+    path('data_entry_page_hindi', views.data_entry_hindi,name="data_entry_hindi"),
+    path('register_hindi/', views.register_hindi),
+    path('user_logout_hindi/', views.user_logout_hindi, name="user_logout_hindi"),
+    
  ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- sankalan/sankalan_app/views.py
+++ sankalan/sankalan_app/views.py
@@ -145,3 +145,117 @@ def check_user(request):
 			return HttpResponse("exists")
 		else:
 			return HttpResponse("not exists")
+
+# Hindi
+
+def index_hindi(request):
+
+	if request.method == "POST":
+
+		if 'login_form' in request.POST:
+
+			username = request.POST["username"]
+			password = request.POST["password"]
+
+			user = authenticate(username=username, password=password)
+
+			if user:
+				login(request,user)
+				return HttpResponseRedirect("/data_entry_page_hindi")
+			else:
+				return render(request,'index_hindi.html',{"status":"अमान्य उपयोगकर्ता नाम या पासवर्ड!"})
+
+		if 'contact_form' in request.POST:
+
+			fname = request.POST["fname"]
+			lname = request.POST["lname"]
+			state = request.POST["state"]
+			email = request.POST["email"]
+			subject = request.POST["subject"]
+
+			data = Contact(fname=fname,lname=lname,state=state,email=email,subject=subject)
+			data.save()
+
+			return render(request,'contact_form_redirect_hindi.html')
+
+		elif 'feedback_form' in request.POST:
+
+			fname = request.POST["fname"]
+			lname = request.POST["lname"]
+			email = request.POST["email"]
+			subject = request.POST["subject"]
+			rating = request.POST["rating"]
+		
+			data = Feedback(fname=fname,lname=lname,email=email,subject=subject,rating=rating)
+			data.save()
+
+			return render(request,'feedback_form_redirect_hindi.html')
+
+	return render(request,'index_hindi.html')
+
+@login_required
+def data_entry_hindi(request):
+
+	surveyor_data = Surveyor.objects.get(user__id=request.user.id)
+	context = {}
+	context["surveyor_data"] = surveyor_data
+
+	if 'data_entry_form' in request.POST:
+
+		fname = request.POST["fname"]
+		lname = request.POST["lname"]
+		aadhaar_no = request.POST["aadhaar_no"]
+		dob = request.POST["dob"]
+		sex = request.POST["sex"]
+		email = request.POST["email"]
+		mobile_no = request.POST["mobile_no"]
+		address = request.POST["address"]
+		city = request.POST["city"]
+		state = request.POST["state"]
+		country = request.POST["country"]
+		occupation = request.POST["occupation"]
+		family_members = request.POST["family_members"]
+
+		data = Civilian_data(fname=fname,lname=lname,aadhaar_no=aadhaar_no,dob=dob,sex=sex,email=email,mobile_no=mobile_no,address=address,city=city,state=state,country=country,occupation=occupation,family_members=family_members)
+		data.save()
+
+		return render(request,'data_entry_form_redirect_hindi.html')
+
+	return render(request,'data_entry_hindi.html',context)
+
+@login_required
+def user_logout_hindi(request):
+	logout(request)
+	return HttpResponseRedirect("/homepage_hindi")
+
+def register_hindi(request):
+
+	if 'registration_form' in request.POST:
+		fname = request.POST["fname"]
+		lname = request.POST["lname"]
+		username = request.POST["username"]
+		surveyor_id = request.POST["surveyor_id"]
+		aadhaar_no = request.POST["aadhaar_no"]
+		sex = request.POST["sex"]
+		email = request.POST["email"]
+		contact = request.POST["contact"]
+		address = request.POST["address"]
+		city = request.POST["city"]
+		state = request.POST["state"]
+		country = request.POST["country"]
+		password = request.POST["password"]
+
+		user = User.objects.create_user(username,email,password)
+		user.first_name = fname
+		user.last_name = lname
+		user.save()
+
+		if "photo" in request.FILES:
+			photo = request.FILES["photo"]
+
+		data = Surveyor(user=user,fname=fname,lname=lname,email=email,surveyor_id=surveyor_id,contact=contact,aadhaar_no=aadhaar_no,photo=photo,sex=sex,address=address,city=city,state=state,country=country)
+		data.save()
+
+		return render(request,'register_redirect_hindi.html')
+
+	return render(request,'register_hindi.html')
