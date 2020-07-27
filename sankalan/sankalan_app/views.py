03634f9dd4884eb8409c71caa4a1802564618801
SristiV <sristiv20@gmail.com>
2020-07-28 00:31:12 +0530

    indentation error solved in views.py file line 30

--- sankalan/sankalan_app/views.py
+++ sankalan/sankalan_app/views.py
@@ -27,9 +27,10 @@ def index(request):
 			if user and not user.is_staff:
 				login(request,user)
 				return HttpResponseRedirect("/data_entry_page")
-            elif user and user.is_staff:
+			elif user and user.is_staff:
 				login(request,user)
 				return HttpResponseRedirect("/register")
+
 			else:
 				return render(request,'index.html',{"status":"Invalid username or password!"})
 
